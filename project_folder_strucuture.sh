e406b1e5250d9747659c26c925eb1ba353a8625e
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-16 16:51:58 +0100

    Add script to create project folder structure

---
+++ project_folder_strucuture.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Create directories
+mkdir -p config/docker \
+		 data/raw data/processed data/metadata \
+		 notebooks/exploratory notebooks/final notebooks/archive \
+		 scripts/R scripts/python scripts/bash \
+		 workflows/modules \
+		 results/tables results/figures results/logs \
+		 docs \
+		 tests/data_tests tests/workflow_tests \
+		 .github/workflows \
+		 logs
+
+# Notify completion
+echo "Folder structure created successfully."
