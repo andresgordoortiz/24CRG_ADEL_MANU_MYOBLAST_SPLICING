9f5d5656b96c1fcc8b03e58dba3ddf529cb17df1
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-17 13:01:47 +0100

    Update VAST alignment script to use raw input files instead of trimmed files

--- scripts/bash/zhang_nature/vast_align_zhang.sh
+++ scripts/bash/zhang_nature/vast_align_zhang.sh
@@ -43,11 +43,11 @@ set -o pipefail
 ###############
 
 #Define file list and select the file for the current array job
-files=($PWD/data/processed/zhang_nature/*_1_trimmed.fastq.gz)
+files=($PWD/data/raw/zhang_nature/*_1.fastq.gz)
 file1=${files[$SLURM_ARRAY_TASK_ID]}
-file2=${file1/_1_trimmed.fastq.gz/_2_trimmed.fastq.gz}
+file2=${file1/_1.fastq.gz/_2.fastq.gz}
 
-basename=$(basename "$file1" _1_trimmed.fq.gz)
+basename=$(basename "$file1" _1.fastq.gz)
 mkdir -p $PWD/data/processed/zhang_nature/vast_out
 
 # Initialize conda
