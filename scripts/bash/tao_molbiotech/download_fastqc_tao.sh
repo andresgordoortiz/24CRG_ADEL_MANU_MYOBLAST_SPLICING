1cb98f93e022f24846652d9ef8c660db4ac8219d
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-17 14:39:48 +0100

    Add SLURM script for downloading FastQC files for Tao Molbiotech dataset

---
+++ scripts/bash/tao_molbiotech/download_fastqc_tao.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+##################
+# slurm settings #
+##################
+
+# where to put stdout / stderr
+#SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.out
+#SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.err
+
+# time limit in minutes
+#SBATCH --time=3
+
+# queue
+#SBATCH --qos=vshort
+
+# memory (MB)
+#SBATCH --mem=2G
+
+# job name
+#SBATCH --job-name downloadfasta
+
+# job array directive
+#SBATCH --array=0-23
+
+#################
+# start message #
+#################
+start_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
+
+##################################
+# make bash behave more robustly #
+##################################
+set -e
+set -u
+set -o pipefail
+
+###############
+# run command #
+###############
+mkdir -p $PWD/data/raw/tao_molbiotech
+cd $PWD/data/raw/tao_molbiotech
+
+# Check if there are less than 24 fastq.gz files
+file_count=$(ls -1 *.fastq.gz 2>/dev/null | wc -l)
+if [ "$file_count" -lt 24 ]; then
+    sed "$((SLURM_ARRAY_TASK_ID + 1))q;d" "$PWD/$1" | bash
+else
+    echo "There are already 24 or more fastq.gz files in the directory."
+fi
+
+###############
+# end message #
+###############
+end_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] finished on $(hostname) after $((end_epoch-start_epoch)) seconds
