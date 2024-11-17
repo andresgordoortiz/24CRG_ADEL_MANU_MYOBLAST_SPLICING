bed207f4c0e54a12dec5399844e590d39dd0c444
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-17 18:40:14 +0100

    Update zip command in run_fastqc_tao.sh to use absolute path for output file

--- scripts/bash/tao_molbiotech/run_fastqc_tao.sh
+++ scripts/bash/tao_molbiotech/run_fastqc_tao.sh
@@ -53,7 +53,7 @@ module load MultiQC/1.22.3-foss-2023b
 multiqc . -n tao_molbiotech_multiqc_report.html
 
 # Zip every file containing fastqc in the processed folder
-zip fastqc_results_tao.zip *fastqc*
+zip $PWD/fastqc_results_tao.zip *fastqc*
 rm *fastqc*
 # end message #
 ###############
