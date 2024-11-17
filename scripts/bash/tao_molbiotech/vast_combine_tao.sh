e2e67960179ddb5b4430c4a62ef5fe9aa9ae56db
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-17 13:31:59 +0100

    Add SLURM script for combining VAST outputs on Tao Molbiotech dataset

---
+++ scripts/bash/tao_molbiotech/vast_combine_tao.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+
+##################
+# slurm settings #
+##################
+
+# where to put stdout / stderr
+#SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.out
+#SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.err
+
+# time limit in minutes
+#SBATCH --time=45
+
+# queue
+#SBATCH --qos=vshort
+
+# memory (MB)
+#SBATCH --mem=5G
+
+# job name
+#SBATCH --job-name vast-combine
+
+
+#################
+# start message #
+#################
+start_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
+
+##################################
+# make bash behave more robustly #
+##################################
+set -e
+set -u
+set -o pipefail
+
+
+###############
+# run command #
+###############
+# Store current working directory
+current_dir=$PWD
+cd $PWD/data/processed/tao_molbiotech/vast_out/to_combine
+
+# Initialize conda
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate vasttools
+
+/users/mirimia/projects/vast-tools/vast-tools combine \
+    -sp mm10 \
+    -o $current_dir/data/processed/tao_molbiotech/vast_out
+
+conda deactivate
+
+
+
+###############
+# end message #
+###############
+end_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] finished on $(hostname) after $((end_epoch-start_epoch)) seconds
