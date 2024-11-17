53e7c8d402888632195d8153a87ad27d9ddcf397
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-17 13:31:50 +0100

    Add SLURM script for running FastQC and MultiQC on Tao Molbiotech dataset

---
+++ scripts/bash/tao_molbiotech/run_fastqc_tao.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+##################
+# slurm settings #
+##################
+
+# where to put stdout / stderr
+#SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/tmp2/%x.%A_%a.out
+#SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/tmp2/%x.%A_%a.err
+
+# time limit in minutes
+#SBATCH --time=60
+
+# queue
+#SBATCH --qos=vshort
+
+# memory (MB)
+#SBATCH --mem=5G
+#SBATCH --cpus-per-task=8
+
+# job name
+#SBATCH --job-name fastqc_multiqc
+
+#################
+# start message #
+#################
+start_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
+
+##################################
+# make bash behave more robustly #
+##################################
+set -e
+set -u
+set -o pipefail
+
+
+################
+# run fastqc   #
+################
+mkdir -p $PWD/tmp2/fastqc
+# Run FastQC using Singularity
+singularity exec --bind $PWD/data/raw/tao_molbiotech \
+    docker://biocontainers/fastqc:v0.11.9_cv8 \
+    fastqc -t 8 $PWD/data/raw/tao_molbiotech/*.fastq.gz \
+    --outdir $PWD/tmp2/fastqc
+
+################
+# run multiqc  #
+################
+cd $PWD/tmp2/fastqc
+module load MultiQC/1.22.3-foss-2023b
+multiqc .
+
+###############
+# end message #
+###############
+end_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] finished on $(hostname) after $((end_epoch-start_epoch)) seconds
