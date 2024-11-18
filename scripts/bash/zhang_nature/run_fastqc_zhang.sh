10e1dc934d44e5215eeb871aa6308b85ab9458a5
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-18 09:42:33 +0100

    Fix zip command output filename case in run_fastqc_zhang.sh

--- scripts/bash/zhang_nature/run_fastqc_zhang.sh
+++ scripts/bash/zhang_nature/run_fastqc_zhang.sh
@@ -53,7 +53,7 @@ module load MultiQC/1.22.3-foss-2023b
 multiqc . -n zhang_nature_multiqc_report.html
 
 # Zip every file containing fastqc in the processed folder
-zip $PWD/fastqc_results_zhang.zip *fastqc*
+zip $PWD/fastQC_results_zhang.zip *fastqc*
 rm *fastqc*
 
 ###############
