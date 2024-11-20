f9e5ade6ea5e2b4ca959098973fd5aa696c3cfe0
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-20 14:37:28 +0100

    update SLURM script to reorder jobs for Trapnell 2010 dataset processing pipeline

--- workflows/full_processingpipeline_trapnell.sh
+++ workflows/full_processingpipeline_trapnell.sh
@@ -4,25 +4,33 @@
 # Full Processing Pipeline CRG Adel Lab for Trapnell 2010 dataset
 # This script processes the data by performing the following steps:
 # 1. Concatenate reads
-# 2. Trim reads
 # 3. Align reads
 # 4. Generate a multiQC report
 # 5. Run vast combine
-# 6. Run vast compare
 ##############################
 
 # SLURM output and error files
 #SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.out
 #SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.err
 
-# Third job - align reads
-echo "Submitting third job: Align reads..."
-jid1=$(sbatch $PWD/scripts/bash/vast_align_trapnell.sh | tr -cd '[:digit:].')
-echo "...third job ID is $jid1"
+# First job - downlaod files
+echo "Submitting first job: download files..."
+jid1=$(sbatch $PWD/scripts/bash/trapnell/download_fastq_trapnell.sh | tr -cd '[:digit:].')
+echo "...first job ID is $jid1"
 
-# Fifth job - run vast combine (dependent on third job)
-echo "Submitting fifth job: Run vast combine..."
-jid2=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/vast_combine_trapnell.sh | tr -cd '[:digit:].')
-echo "...fifth job ID is $jid2"
+# Second job - run fastqc and multiqc
+echo "Submitting second job: fastqc..."
+jid2=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/trapnell/run_fastqc_trapnell.sh | tr -cd '[:digit:].')
+echo "...second job ID is $jid2"
+
+# Third job - vast align
+echo "Submitting third job: vast align..."
+jid3=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/trapnell/vast_align_trapnell.sh | tr -cd '[:digit:].')
+echo "...third job ID is $jid3"
+
+# Fourth job - vast combine
+echo "Submitting fourth job: vast combine..."
+jid4=$(sbatch --dependency=afterok:$jid3 $PWD/scripts/bash/trapnell/vast_combine_trapnell.sh | tr -cd '[:digit:].')
+echo "...third job ID is $jid4"
 
 echo "All jobs submitted!"
