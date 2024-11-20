1a6d0a646dc0c83dd5ef3287ec44cc5cd2e2afb9
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-20 10:31:56 +0100

    add SLURM script for running FastQC and MultiQC on lyu_cells2022 data

---
+++ scripts/bash/lyu_cells2022/run_fastqc_lyu.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+##################
+# slurm settings #
+##################
+
+# where to put stdout / stderr
+#SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.out
+#SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.err
+
+# time limit in minutes
+#SBATCH --time=60
+
+# queue
+#SBATCH --qos=vshort
+
+# memory (MB)
+#SBATCH --mem=5G
+#SBATCH --cpus-per-task=8
+
+# job name
+#SBATCH --job-name fastqc_multiqc
+
+#################
+# start message #
+#################
+start_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
+
+##################################
+# make bash behave more robustly #
+##################################
+set -e
+set -u
+set -o pipefail
+
+
+################
+# run fastqc   #
+################
+mkdir -p $PWD/data/processed/lyu_cells2022
+# Run FastQC using Singularity
+singularity exec --bind $PWD/data/raw/lyu_cells2022 \
+    docker://biocontainers/fastqc:v0.11.9_cv8 \
+    fastqc -t 8 $PWD/data/raw/lyu_cells2022/*.fastq.gz \
+    --outdir $PWD/data/processed/lyu_cells2022
+
+################
+# run multiqc  #
+################
+cd $PWD/data/processed/lyu_cells2022
+module load MultiQC/1.22.3-foss-2023b
+multiqc . -n lyu_cells_multiqc_report.html
+
+# Zip every file containing fastqc in the processed folder
+zip $PWD/fastQC_results_lyu.zip *fastqc*
+rm *fastqc*
+
+###############
+# end message #
+###############
+end_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] finished on $(hostname) after $((end_epoch-start_epoch)) seconds
