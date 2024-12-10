cbdd5e4c4d528963f1bd7e63b2b3d695d02d4355
andresgordoortiz <andresgordoortiz@gmail.com>
2024-12-10 16:29:13 +0100

    Add script to download FASTQ files for SRR908 study

---
+++ data/raw/christopher_physioreports/christopher_fastqfiles.sh
@@ -0,0 +1,6 @@
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR908/001/SRR9089321/SRR9089321.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR908/000/SRR9089320/SRR9089320.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR908/003/SRR9089323/SRR9089323.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR908/002/SRR9089322/SRR9089322.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR908/004/SRR9089324/SRR9089324.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR908/009/SRR9089319/SRR9089319.fastq.gz
