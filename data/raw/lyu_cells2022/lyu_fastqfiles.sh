6742c2c46e6bcae63eb99eb33209bd8fdcdebf59
andresgordoortiz <andresgordoortiz@gmail.com>
2024-12-10 16:29:21 +0100

    Add script to download FASTQ files for SRR157 study

---
+++ data/raw/lyu_cells2022/lyu_fastqfiles.sh
@@ -0,0 +1,8 @@
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR157/048/SRR15731248/SRR15731248_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR157/049/SRR15731249/SRR15731249_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR157/049/SRR15731249/SRR15731249_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR157/050/SRR15731250/SRR15731250_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR157/047/SRR15731247/SRR15731247_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR157/048/SRR15731248/SRR15731248_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR157/047/SRR15731247/SRR15731247_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR157/050/SRR15731250/SRR15731250_1.fastq.gz
