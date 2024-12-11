81e43766e348993443192b8a51ea6197a6908281
andresgordoortiz <andresgordoortiz@gmail.com>
2024-12-11 12:52:00 +0100

    Add VASTDB_PATH argument to full_processingpipeline_trapnell.sh for improved job submission

--- workflows/full_processingpipeline_trapnell.sh
+++ workflows/full_processingpipeline_trapnell.sh
@@ -13,6 +13,13 @@
 #SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.out
 #SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.err
 
+if [ -z "$1" ]; then
+    echo "Error: No VASTDB_PATH provided."
+    echo "Usage: $0 /path/to/vastdb"
+    exit 1
+fi
+
+VASTDB_PATH=$1
 # First job - downlaod files
 echo "Submitting first job: download files..."
 jid1=$(sbatch $PWD/scripts/bash/trapnell/download_fastq_trapnell.sh | tr -cd '[:digit:].')
@@ -25,12 +32,12 @@ echo "...second job ID is $jid2"
 
 # Third job - vast align
 echo "Submitting third job: vast align..."
-jid3=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/trapnell/vast_align_trapnell.sh | tr -cd '[:digit:].')
+jid3=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/trapnell/vast_align_trapnell.sh $VASTDB_PATH | tr -cd '[:digit:].')
 echo "...third job ID is $jid3"
 
 # Fourth job - vast combine
 echo "Submitting fourth job: vast combine..."
-jid4=$(sbatch --dependency=afterok:$jid3 $PWD/scripts/bash/trapnell/vast_combine_trapnell.sh | tr -cd '[:digit:].')
+jid4=$(sbatch --dependency=afterok:$jid3 $PWD/scripts/bash/trapnell/vast_combine_trapnell.sh $VASTDB_PATH | tr -cd '[:digit:].')
 echo "...third job ID is $jid4"
 
 echo "All jobs submitted!"
