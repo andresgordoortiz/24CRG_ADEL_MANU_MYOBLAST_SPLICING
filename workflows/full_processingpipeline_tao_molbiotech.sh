0ab65aa93fa85e2effbffcc9c8d7f5349a65de53
andresgordoortiz <andresgordoortiz@gmail.com>
2024-12-11 12:43:39 +0100

    Add VASTDB_PATH parameter to full_processingpipeline_tao_molbiotech.sh and update job submissions

--- workflows/full_processingpipeline_tao_molbiotech.sh
+++ workflows/full_processingpipeline_tao_molbiotech.sh
@@ -14,9 +14,15 @@
 #SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.out
 #SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/logs/%x.%A_%a.err
 
+if [ -z "$1" ]; then
+    echo "Error: No VASTDB_PATH provided."
+    echo "Usage: $0 /path/to/vastdb"
+    exit 1
+fi
 
+VASTDB_PATH=$1
 # First job - Download FastQC
-echo "Submitting first job: FastQC..."
+echo "Submitting first job: Downloading fastq..."
 jid1=$(sbatch $PWD/scripts/bash/tao_molbiotech/download_fastqc_tao.sh /data/raw/tao_molbiotech/fastq_files_tao.sh | tr -cd '[:digit:].')
 echo "...first job ID is $jid1"
 
@@ -27,12 +33,12 @@ echo "...first job ID is $jid2"
 
 # Third job - align reads
 echo "Submitting second job: Align reads..."
-jid3=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/tao_molbiotech/vast_align_tao.sh | tr -cd '[:digit:].')
+jid3=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/tao_molbiotech/vast_align_tao.sh $VASTDB_PATH  | tr -cd '[:digit:].')
 echo "...second job ID is $jid3"
 
 # Fourth job - run vast combine (dependent on second job)
 echo "Submitting third job: Run vast combine..."
-jid4=$(sbatch --dependency=afterok:$jid3 $PWD/scripts/bash/tao_molbiotech/vast_combine_tao.sh | tr -cd '[:digit:].')
+jid4=$(sbatch --dependency=afterok:$jid3 $PWD/scripts/bash/tao_molbiotech/vast_combine_tao.sh $VASTDB_PATH | tr -cd '[:digit:].')
 echo "...third job ID is $jid4"
 
 
