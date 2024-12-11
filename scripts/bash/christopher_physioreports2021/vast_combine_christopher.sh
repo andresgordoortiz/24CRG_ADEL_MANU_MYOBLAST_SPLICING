c515cdff509fc874799bf9059b37093fcba12057
andresgordoortiz <andresgordoortiz@gmail.com>
2024-12-11 09:35:29 +0100

    Update output path for inclusion levels in vast_combine_christopher.sh

--- scripts/bash/christopher_physioreports2021/vast_combine_christopher.sh
+++ scripts/bash/christopher_physioreports2021/vast_combine_christopher.sh
@@ -49,7 +49,7 @@ singularity exec --bind $VASTDB_PATH:/usr/local/vast-tools/VASTDB \
     --bind $PWD/data/processed/christopher_physioreports:/christopher_physioreports \
     $singularity_image bash -c "vast-tools combine /christopher_physioreports/vast_out/to_combine -sp mm10 -o /christopher_physioreports/vast_out"
 
-mv $PWD/data/processed/christopher_physioreports/vast_out/INCLUSION_LEVELS_FULL* $PWD/notebooks/inclusion_tables/Christopher_Physioreports_INCLUSION_LEVELS_FULL-mm10.tab
+mv $PWD/data/processed/christopher_physioreports/vast_out/INCLUSION_LEVELS_FULL* $PWD/notebooks/final/inclusion_tables/Christopher_Physioreports_INCLUSION_LEVELS_FULL-mm10.tab
 
 ###############
 # end message #
