f70251874865c5231ca26cc58e3bf927823a4c81
Andrés Gordo <andresgordoortiz@gmail.com>
2024-12-13 15:49:29 +0100

    Finish script

--- scripts/R/events_finding.R
+++ scripts/R/events_finding.R
@@ -18,24 +18,38 @@ dfs <- list(
   list(df = zhang_pdiff_introns, group = "Introns", study = "Zhang")
 )
 
-# Add metadata columns to each dataframe
+# Add metadata columns to each dataframe beforehand
 dfs_with_metadata <- lapply(dfs, function(x) {
   x$df %>%
     mutate(Group = x$group, Study = x$study)
 })
 
-# Merge all dataframes by EVENT, filling missing columns with NA
-final_df <- Reduce(function(x, y) {
-  full_join(x, y, by = "EVENT")
-}, dfs_with_metadata)
-
-# View the final dataframe
-print(final_df)
+# Combine all dataframes into one using bind_rows() for faster merging
+final_df <- bind_rows(dfs_with_metadata) %>%
+  select(EVENT, Group, Study, deltapsi) %>%
+  filter(abs(deltapsi)>=0.15) %>%
+  na.omit()# Adjust this line if other columns should be prioritized
 
 
+plot_psi_distribution <- ggplot(final_df, aes(x = deltapsi, fill = ifelse(deltapsi < 0, "Skipped", "Included"))) +
+  geom_histogram(alpha = 0.8, bins = 100, position = "identity") +
+  labs(
+    title = "PSI Distribution of all Events",
+    subtitle = "All 5 studies were merged",
+    x = "ΔPSI (Myotubes-Myoblasts)",
+    y = "Density",
+    fill = "|ΔPSI| > 0.15",
+    caption = paste0("Created by AG on ", Sys.Date())
+  ) +
+  theme_minimal(base_family = font) +
+  theme(
+    legend.position = "right",
+    axis.title.x = element_text(margin = margin(t = 10)),
+    axis.text.x = element_text(size = 15),
+    panel.background = element_rect(fill = "white"),
+    panel.grid.major = element_line(color = "gray80", linewidth = 0.5),
+    panel.grid.minor = element_line(color = "gray95", linewidth = 0.3)
+  ) +
+  scale_fill_brewer(palette = "Set1") +
+  facet_wrap(~Group)
 
-ggplot(data, aes(x=deltapsi, fill=ifelse(deltapsi < 0, "Skipped", "Included"))) +
-  geom_histogram(alpha=0.7, bins=100) +
-  theme_minimal() +
-  labs(title="PSI distribution of Exons", x="ΔPSI (Myotubes-Myoblasts)", y="Density", fill="|ΔPSI| > 0.15") +
-  scale_fill_brewer(palette="Set1")
