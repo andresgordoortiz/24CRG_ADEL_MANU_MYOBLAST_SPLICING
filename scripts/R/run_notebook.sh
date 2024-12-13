22dec77191bf0c02e8d76da3594d7b74c928efff
andresgordoortiz <andresgordoortiz@gmail.com>
2024-12-13 15:04:13 +0100

    Download VASTDB files automatically

--- scripts/R/run_notebook.sh
+++ scripts/R/run_notebook.sh
@@ -35,6 +35,35 @@ set -e
 set -u
 set -o pipefail
 
+# Define URLs and filenames
+URL1="https://vastdb.crg.eu/downloads/mm10/SPLICE_SITE_SCORES-mm10.tab.gz"
+URL2="https://vastdb.crg.eu/downloads/mm10/EVENT_INFO-mm10.tab.gz"
+
+FILE1="$PWD/notebooks/final/SPLICE_SITE_SCORES-mm10.tab.gz"
+FILE2="$PWD/notebooks/final/EVENT_INFO-mm10.tab.gz"
+
+# Create the directory if it doesn't exist
+mkdir -p "$PWD/notebooks/final"
+
+# Check and download the first file
+if [ ! -f "$FILE1" ]; then
+    echo "$FILE1 not found. Downloading..."
+    wget "$URL1" -O "$FILE1"
+else
+    echo "$FILE1 already exists. Skipping download."
+fi
+
+# Check and download the second file
+if [ ! -f "$FILE2" ]; then
+    echo "$FILE2 not found. Downloading..."
+    wget "$URL2" -O "$FILE2"
+else
+    echo "$FILE2 already exists. Skipping download."
+fi
+# Unzip the downloaded files
+echo "Unzipping files..."
+gunzip -c "$FILE1" > "${FILE1%.gz}"
+gunzip -c "$FILE2" > "${FILE2%.gz}"
 # Set the working directory inside the container to /workspace
 singularity run --bind "$(pwd)/notebooks/final:/shared" \
   docker://andresgordoortiz/splicing_analysis_r_crg:v1.3 \
