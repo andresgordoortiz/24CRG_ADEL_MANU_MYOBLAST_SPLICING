e6be3237725a2977ddbc9d5aaaaefb0c44b016db
Andrés Gordo <andresgordoortiz@gmail.com>
2025-01-16 14:07:20 +0100

    Update table making

--- scripts/R/clust_table_making.R
+++ scripts/R/clust_table_making.R
@@ -1,6 +1,9 @@
-# Create clust tables
-clust_events_tao<-na.omit(data.frame(ID=tao_events$PSI$EVENT, t0=rowMeans(tao_events$PSI[,7:9]), t2=rowMeans(tao_events$PSI[,10:12]),t4=rowMeans(tao_events$PSI[,13:15]),t7=rowMeans(tao_events$PSI[,16:18])))
-write_tsv(clust_events_tao, "clust_events_tao.txt")
+# Create clust tables gene expression
+
+splicing_factor_genes<-txi$counts[rownames(txi$counts) %in% mouse_sf_filtered,]
+colnames(splicing_factor_genes)<-metadata$run_accession
+clust_events_tao<-na.omit(data.frame(ID=rownames(splicing_factor_genes), t0=rowMeans(splicing_factor_genes[,1:3]), t2=rowMeans(splicing_factor_genes[,4:6]),t4=rowMeans(splicing_factor_genes[,7:9]),t7=rowMeans(splicing_factor_genes[,10:12])))
+write_tsv(clust_events_tao, "clust_events_tao_gene_expression.txt")
 
 clust_events_trapnell<-na.omit(data.frame(ID=trapnell_events$PSI$EVENT, t0=rowMeans(trapnell_events$PSI[,7:9]), t2=rowMeans(trapnell_events$PSI[,10:11]),t4=rowMeans(trapnell_events$PSI[,12:13]),t7=rowMeans(trapnell_events$PSI[,14:16])))
 write_tsv(clust_events_trapnell, "clust_events_trapnell.txt")
