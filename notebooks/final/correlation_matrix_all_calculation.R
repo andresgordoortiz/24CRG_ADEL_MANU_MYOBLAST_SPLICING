6fc529f6fab569c64f655097dc96d37a559ab2e3
Andrés Gordo <andresgordoortiz@gmail.com>
2025-02-19 15:11:23 +0100

    Add full path to splice factors

--- notebooks/final/correlation_matrix_all_calculation.R
+++ notebooks/final/correlation_matrix_all_calculation.R
@@ -31,7 +31,7 @@ tao_events <- filterEvents(getEvents(tao_data, tool = "vast-tools"), N=10) # Ext
 tao_exons <- filterEvents(tao_events, types = c("C1", "C2", "C3", "S", "MIC"), N = 10)
 tao_introns <- filterEvents(tao_events, types = c("IR"), N = 10)
 
-splice_factors<-read_xlsx("SFs_list_ensembl_ID.xlsx")
+splice_factors<-read_xlsx("/users/aaljord/agordo/git/24CRG_ADEL_MANU_MYOBLAST_SPLICING/notebooks/final/SFs_list_ensembl_ID.xlsx")
 
 correlation_matrix<-tao_events$PSI[,-c(3:6)]
 
